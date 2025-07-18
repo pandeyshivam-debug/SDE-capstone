40d0b810db4e758d860f82ca76f6b1bfd733c89b
pandeyshivam-debug <shivam.pandey@minfytech.com>
2025-07-18 16:45:40 +0530

    add editor functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,29 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login" 
 import Dashboard from "./pages/Dashboard" 
 import ProtectedRoute from "./components/ProtectedRoute"
+import Editor from "./pages/Editor"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-          <Dashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/editor/:id"
+          element={
+            <ProtectedRoute>
+              <Editor />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   ) 
