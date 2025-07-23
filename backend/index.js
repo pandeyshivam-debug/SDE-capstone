56a8936b2638d8415d4ea204575edafd2f23e207
pandeyshivam-debug <shivam.pandey@minfytech.com>
2025-07-23 16:30:30 +0530

    doc

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,23 @@
 import express from "express"
 import cors from "cors"
-
+import swaggerUi from "swagger-ui-express"
+import YAML from "yamljs"
 import authRoutes from "./routes/auth.routes.js"
 import errorHandler from "./middleware/error.middleware.js"
 import fileRoutes from "./routes/file.routes.js"
 
-const app = express();
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-// Routes
-app.use('/api/auth', authRoutes);
+const swaggerDocument = YAML.load('./openapi.yaml')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use('/api/auth', authRoutes)
 app.use('/api/files', fileRoutes)
 
-// Global error handler
-app.use(errorHandler);
+app.use(errorHandler)
 
-export default app;
+export default app
